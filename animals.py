07c9fe004241e6a8a8a5271f09282a2876392a30
DESKTOP-QSPRG4A\sigvs <sigvsore@nmbu.no>
2018-01-08 12:35:04 +0100

    add animals, landscape, simulation and test files. add first test of animals.

---
+++ animals.py
@@ -0,0 +1,33 @@
+class Animal:
+    def __init__(self):
+        self.age = 0
+
+    def aging(self):
+        self.age += 1
+
+    def weightloss(self):
+        pass
+
+    def movement(self):
+        pass
+
+    def death(self):
+        pass
+
+    def birth(self):
+        pass
+
+    def eating(self):
+        pass
+
+
+def Herbivore(Animal):
+    def __init__(self, zeta, xi):
+        self.weight = weight
+
+
+def Carnivore(Animal):
+    def __init__(self, a_half, phi_age, omega, F, DeltaPhiMax):
+        self.weight = weight
+
+
---
+++ test.py
@@ -0,0 +1,5 @@
+from animals import *
+
+def test_animals():
+    animal = Animal()
+    assert animal.age == 0
