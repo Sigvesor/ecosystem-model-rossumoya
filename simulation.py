ac4644cb1dfc8f38a2d876dec6900d9f5f25b4e4
Filip Rotnes <filip.rotnes@gmail.com>
2018-01-10 11:59:12 +0100

    Add outline for simulation module

--- simulation.py
+++ simulation.py
@@ -1,2 +1,32 @@
-# ANIMAL CLASSES:
+# -*- coding: utf-8 -*-
 
+__author__ = 'Sigve Sorensen', 'Filip Rotnes'
+__email__ = 'sigvsore@nmbu.no', 'firo@nmbu.no'
+
+import pandas as pd
+from landscape import *
+from animals import *
+"""
+plan:
+
+make map
+make animals and place
+for _ in nrounds:
+    1. order by fitness
+    2. for animal:
+            sim_eat(animal)
+            update phi
+    3. mate and add new animals
+    4. if animal.phi !=  None:
+        ageing()
+        weightloss()
+        death()
+    update phi
+    regenerate fodder
+"""
+
+def jungle_constructor():
+    pass
+
+def Bio_Sim():
+    island_map = Jungle()
