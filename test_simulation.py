078c16c57df342d70710b3ea50faa4913dbb86dd
Filip Rotnes <filip.rotnes@gmail.com>
2018-01-10 11:58:36 +0100

    Add outline for tests of simulation

--- test_simulation.py
+++ test_simulation.py
@@ -3,5 +3,52 @@
 __author__ = 'Sigve Sorensen', 'Filip Rotnes'
 __email__ = 'sigvsore@nmbu.no', 'firo@nmbu.no'
 
-def test_simulation():
+from simulation import *
+
+def test_map_construction():
+    """
+    is the map constructed correctly?
+    is number of elements correct?
+    is the island framed by ocean?
+    does the type of each element correspond to the input?
+    :return:
+    """
+    # assert type(island_map) == pd.core.frame.DataFrame
     pass
+
+def test_animal_placement():
+    """
+    is the right number of animals created?
+
+    :return:
+    """
+    pass
+
+def test_eating():
+    """
+    the animal is eating if fodder in cell. the fodder of the cell is reduced,
+    weight of animal increased.
+    """
+    pass
+
+def test_eating_order():
+    """
+    are the animals getting ordered so that the fittest get to feed
+    and the unfit starve?
+    :return:
+    """
+    pass
+
+def test_population_growth():
+    """
+    is the population growing in an resource rich environment?
+    give rich environment and count number of individuals
+    :return:
+    """
+    pass
+
+def test_population_decline():
+    """ test that the population declines as a result of death due to
+    limited resources, and that the list of animals declines"""
+    pass
+
