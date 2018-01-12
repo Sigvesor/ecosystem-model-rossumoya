c6b3c1cc69169cb4e64ef2ab4cb6f952489b404c
DESKTOP-QSPRG4A\sigvs <sigvsore@nmbu.no>
2018-01-12 15:09:10 +0100

    update function names

--- test_animals.py
+++ test_animals.py
@@ -6,24 +6,26 @@ __email__ = 'sigvsore@nmbu.no', 'firo@nmbu.no'
 
 import animals
 
+
 def test_biocycle():
     herbivore = animals.Herbivore()
     assert herbivore.age == 0
-    herbivore.aging()
-    w_before = herbivore.w
+    herbivore.ages()
+    w_before = herbivore.weight
     herbivore.eating(100)
-    assert herbivore.w > w_before
+    assert herbivore.weight > w_before
     assert herbivore.age == 1
-    w_before = herbivore.w
+    w_before = herbivore.weight
     herbivore.weightloss()
-    assert herbivore.w < w_before
+    assert herbivore.weight < w_before
+
 
 def test_fitness():
     herbivore = animals.Herbivore()
     herbivore.fitness()
     fit1 = herbivore.phi
     assert 0 <= fit1 <= 1
-    herbivore.aging()
+    herbivore.ages()
     herbivore.fitness()
     fit2 = herbivore.phi
     assert fit1 > fit2
@@ -31,6 +33,7 @@ def test_fitness():
     herbivore.fitness()
     fit3 = herbivore.phi
     assert fit2 > fit3
+
 
 def test_birth():
     herbivore = animals.Herbivore()
@@ -41,10 +44,10 @@ def test_birth():
 
 def test_death():
     herbivore = animals.Herbivore()
-    herbivore.param['omega'] = 1
+    herbivore.default_params['omega'] = 1
     herbivore.phi = 0
-    assert herbivore.death()
+    assert herbivore.dies()
     herbivore.phi = 1
-    assert not herbivore.death()
+    assert not herbivore.dies()
 
 
