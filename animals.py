5f92d3043f2acddac51aab93ab2dd326727b6a6f
DESKTOP-QSPRG4A\sigvs <sigvsore@nmbu.no>
2018-01-12 15:11:40 +0100

    add classmethod to set parameters on class level add classmethod to show parameters on class level add docstrings on class methods

--- animals.py
+++ animals.py
@@ -1,55 +1,127 @@
 # -*- coding: utf-8 -*-
 
-import random
-from math import exp as e
-
 __author__ = 'Sigve Sorensen', 'Filip Rotnes'
 __email__ = 'sigvsore@nmbu.no', 'firo@nmbu.no'
 
+import random
+from math import exp as e
+
 
 class Herbivore:
+    """Herbiwars which mate, multiply and die with fixed probabilities."""
 
-    param = {'w_birth': 8.0, 'sigma_birth': 1.5, 'beta': 0.9,
-             'a_half': 40.0, 'phi_age': 0.2, 'w_half': 10.0,
-             'phi_weight': 0.1, 'mu': 0.25, 'lambda': 1.0,
-             'gamma': 0.2, 'zeta': 3.5, 'xi': 1.2,
-             'omega': 0.4, 'F': 10.0, 'eta': 0.05}
-
-    def __init__(self, weight=8.0, age=0):
-        self.age = age
-        self.w = weight
-        self.phi = None
+    default_params = {'w_birth': 8.0, 'sigma_birth': 1.5, 'beta': 0.9,
+                      'a_half': 40.0, 'phi_age': 0.2, 'w_half': 10.0,
+                      'phi_weight': 0.1, 'mu': 0.25, 'lambda': 1.0,
+                      'gamma': 0.2, 'zeta': 3.5, 'xi': 1.2,
+                      'omega': 0.4, 'F': 10.0, 'eta': 0.05}
 
     @classmethod
-    def set_parameters(self, parameters=param):
-        for parameter in parameters:
-            self.param[parameter] = parameters[parameter]
+    def set_params(cls, new_params):
+        """
+        Set class parameters.
 
-    def aging(self):
+        Parameters
+        ----------
+        new_params : dict
+            Legal keys: 'w_birth', 'sigma_birth', 'beta', 'a_half',
+                        'phi_age', 'w_half', 'phi_weight', 'mu', 'lambda',
+                        'gamma', 'zeta', 'xi', 'omega', 'F', 'eta'
+
+        Raises
+        ------
+        ValueError, KeyError
+        """
+
+        params = ('w_birth', 'sigma_birth', 'beta', 'a_half',
+                  'phi_age', 'w_half', 'phi_weight', 'mu', 'lambda',
+                  'gamma', 'zeta', 'xi', 'omega', 'F', 'eta')
+
+        for key in new_params:
+            if key not in params:
+                raise KeyError('Invalid parameter name: ' + key)
+
+            if key in params:
+            #    if not type(default_params[key]):
+            #        raise ValueError('Invalid type of inserted key value')
+#
+                cls.default_params[key] = new_params[key]
+
+    @classmethod
+    def get_params(cls):
+        """
+        Get class parameters.
+
+        Returns
+        -------
+        dict
+            Dictionary with class parameters.
+        """
+
+        return cls.default_params
+
+    def __init__(self, weight=8.0, age=0.0):
+        """Create Herbivore with age 0."""
+        self.weight = weight
+        self.age = age
+        self.phi = None
+
+    def ages(self):
+        """Herbivoar ages by one cycle."""
         self.age += 1
 
+    def dies(self):
+        """
+        Decide if Herbivore dies.
+
+        Returns
+        -------
+        bool
+            True if Herbivore dies.
+        """
+
+        p_death = self.default_params['omega'] * (1 - self.phi)
+        return random.random() < p_death
+
     def fitness(self):
-        self.phi = 1 / (1+e(self.param['phi_age']*(self.age - self.param['a_half'])))\
-                   * 1 / (1+e(-self.param['phi_weight']*(self.w - self.param['w_half'])))
+        """
+        Calculates the fitness of the Herbivoar.
+
+        Returns
+        -------
+        float
+            Between [0, 1]
+        """
+        self.phi = 1 / (1+e(self.default_params['phi_age']*(self.age - self.default_params['a_half'])))\
+            * 1 / (1+e(-self.default_params['phi_weight']*(self.weight - self.default_params['w_half'])))
         return self.phi
 
-    def weightloss(self):
-        # weight reduced by mu/year
-        self.w -= self.param['eta'] * self.w
+    def birth(self, pop_herbs):
+        """
+        Decide whether a Herbivore will give birth.
 
-    def movement(self):
-        pass
+        Returns
+        -------
+        bool
+            True if Herbivore gives birth.
+        """
 
-    def death(self):
-        p_death = self.param['omega'] * (1 - self.phi)
-        return random.random() <= p_death
-
-    def birth(self, animals_in_cell):
-        p_of_birth = min([1, self.param['gamma']*self.phi*(animals_in_cell-1)])
+        p_of_birth = min([1, self.default_params['gamma'] * self.phi * (pop_herbs - 1)])
         return random.random() <= p_of_birth
 
-    def eating(self, available_fodder): # må vite hva som er i ruta (f), beta, F
-        self.w += available_fodder * self.param['beta']
+    def eating(self, available_fodder):
+        """
+        Updates the weight of Herbivore after eating.
+        """
+
+        self.weight += available_fodder * self.default_params['beta']
+
+    def weightloss(self):
+        """
+        Updates the weight following weightloss of Herbivore in a cycle.
+        """
+        self.weight -= self.default_params['eta'] * self.weight
+
 
 
 
