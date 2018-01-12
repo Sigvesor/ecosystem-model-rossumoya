09e5dc9de039a350d0c5fff203d926f261b066bb
DESKTOP-QSPRG4A\sigvs <sigvsore@nmbu.no>
2018-01-12 15:15:42 +0100

    add classmethod to set parameters on class level add classmethod to show parameters on class level add docstrings on class methods add functions for elementary functionality of the animal class in the enviorment

--- landscape.py
+++ landscape.py
@@ -3,45 +3,117 @@
 __author__ = 'Sigve Sorensen', 'Filip Rotnes'
 __email__ = 'sigvsore@nmbu.no', 'firo@nmbu.no'
 
+from animals import Herbivore
 
-#class Landscape(object):
-#
-#    def __init__(self, x_val, y_val):
-#        self.x_val = x_val
-#        self.y_val = y_val
-#
-#
-#class Ocean(Landscape):
-#
-#
-#class Mountains(Landscape):
-#
-#
+
 class Jungle:
+    """A jungle containing Herbivoars"""
 
-    param = {'f_max': 800.0}
+    f_max = 800.0
 
-    def __init__(self):
-        self.f = self.param['f_max']
-
-    def eat_request(self, amount):
-        if self.f >= amount:
-            self.f -= amount
-            return amount
-        else:
-            remaining_amount = self.f
-            self.f = 0
-            return remaining_amount
-
-    def regenerate(self):
-        self.f = self.param['f_max']
+    default_params = {'f_max': f_max}
 
     @classmethod
-    def set_parameters(self, parameters=param):
-        for parameter in parameters:
-            self.param[parameter] = parameters[parameter]
+    def set_params(cls, new_params=default_params):
 
+        """
+        Set class parameters.j
 
-#class Savannah(Landscape):
-#    def __init__(self, f_max=300.0):
-#        self.f = f_max
+        Parameters
+        -------
+        new_params : dict
+            Legal keys: 'f_max'
+
+        Raises
+        -------
+        ValueError, KeyError
+        """
+
+        for key in new_params:
+            if key not in 'f_max':
+                raise KeyError('Invalid parameter name: ' + key)
+
+            if key in new_params:
+                cls.default_params[key] = new_params[key]
+
+    @classmethod
+    def get_params(cls):
+        """
+        Get class parameters
+
+        Returns
+        -------
+        dict
+            Dictionary with class parameters.
+        """
+
+        return {'f_max': cls.default_params['f_max']}
+
+    def __init__(self, num_herbs):
+        """
+        Parameters#
+        ----------
+        num_herbs : int
+            number of Herbivores in the jungle.
+        """
+
+        self.f_max = Jungle.default_params['f_max']
+        self.f = self.f_max
+        self.pop_herbs = [Herbivore() for _ in range(num_herbs)]
+
+    def get_num_herbs(self):
+        """Return number of Herbivores in Jungle"""
+
+        return len(self.pop_herbs)
+
+    def aging(self):
+        """Age all Herbivores in Jungle by one cycle."""
+
+        for herb in self.pop_herbs:
+            herb.ages()
+
+    def death(self):
+        """Remove dying Herbivores."""
+
+        def survivors(pop):
+            return [herb for herb in pop if not herb.dies()]
+
+        self.pop_herbs = survivors(self.pop_herbs)
+
+    def reproduction(self):
+        """For each Herbivore reproducing, add one new."""
+
+        def newborns(pop):
+            return [Herbivore() for herb in pop if herb.birth(len(pop))]
+
+        self.pop_herbs.extend(newborns(self.pop_herbs))
+
+    def weightloss(self):
+        for herb in self.pop_herbs:
+            herb.weightloss()
+
+    def fitness_sort(self):
+        for herb in self.pop_herbs:
+            herb.fitness()
+
+        asc = False
+        while not asc:
+            asc = True
+            for herb in range(len(self.pop_herbs) - 1):
+                if self.pop_herbs[herb].phi < self.pop_herbs[herb + 1].phi:
+                    self.pop_herbs[herb], self.pop_herbs[herb + 1] = self.pop_herbs[herb + 1], self.pop_herbs[herb]
+                    asc = False
+        return self.pop_herbs
+
+    def eat_request(self):
+        for herb in self.pop_herbs:
+            request = herb.default_params['F']
+            if request <= self.f:
+                self.f -= request
+            else:
+                request = self.f
+                self.f = 0
+            herb.eating(request)
+
+    def regenerate(self):
+        self.f = self.f_max
