78b82903333c9f6f59b1cc166552f466a70e79a4
Filip Rotnes <filip.rotnes@gmail.com>
2018-01-23 23:42:19 +0100

    Add the proper map from check_sim to __main__ method

--- biosim/simulation.py
+++ biosim/simulation.py
@@ -4,6 +4,9 @@ import matplotlib.pyplot as plt
 #from biosim.landscape import *
 import random
 from island import *
+from landscape import *
+from animals import *
+from map_constructor import *
 
 __author__ = 'Sigve Sorensen', 'Filip Rotnes'
 __email__ = 'sigvsore@nmbu.no', 'firo@nmbu.no'
@@ -40,25 +43,25 @@ class BioSim:
         plt.show()
 
 
+
 if __name__ == "__main__":
     import timeit
 
     t0 = timeit.default_timer()
 
     kart = """OOOOOOOOOOOOOOOOOOOOO
-    OOOOOOOOSMMMMJJJJJJJO
-    OSSSSSJJJJMMJJJJJJJOO
-    OSSSSSSSSSMMJJJJJJOOO
-    OSSSSSJJJJJJJJJJJJOOO
-    OSSSSSJJJDDJJJSJJJOOO
-    OSSJJJJJDDDJJJSSSSOOO
-    OOSSSSJJJDDJJJSOOOOOO
-    OSSSJJJJJDDJJJJJJJOOO
-    OSSSSJJJJDDJJJJOOOOOO
-    OOSSSSJJJJJJJJOOOOOOO
-    OOOSSSSJJJJJJJOOOOOOO
-    OOOOOOOOOOOOOOOOOOOOO"""
-
+        OOOOOOOOSMMMMJJJJJJJO
+        OSSSSSJJJJMMJJJJJJJOO
+        OSSSSSSSSSMMJJJJJJOOO
+        OSSSSSJJJJJJJJJJJJOOO
+        OSSSSSJJJDDJJJSJJJOOO
+        OSSJJJJJDDDJJJSSSSOOO
+        OOSSSSJJJDDJJJSOOOOOO
+        OSSSJJJJJDDJJJJJJJOOO
+        OSSSSJJJJDDJJJJOOOOOO
+        OOSSSSJJJJJJJJOOOOOOO
+        OOOSSSSJJJJJJJOOOOOOO
+        OOOOOOOOOOOOOOOOOOOOO"""
 
     ini_herbs = [{'loc': (2, 20),
                   'pop': [{'species': 'Herbivore', 'age': 5,
@@ -67,9 +70,10 @@ if __name__ == "__main__":
                   'pop': [{'species': 'Carnivore', 'age': 5,
                            'weight': 20} for _ in range(40)]}]
     sim = BioSim(ini_pop=ini_herbs, island_map=kart, seed=12634)
-    sim.simulate(30)
+
+    sim.simulate(100)
     sim.add_population(ini_carns)
-    sim.simulate(40)
+    sim.simulate(400)
     sim.sim_plot()
     print(timeit.default_timer() - t0)
     # plt.show()
