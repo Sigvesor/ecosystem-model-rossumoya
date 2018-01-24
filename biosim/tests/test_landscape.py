66cce59367f1a8d6eeb67a27c6839e48eddd49ba
DESKTOP-QSPRG4A\sigvs <sigvsore@nmbu.no>
2018-01-24 01:50:16 +0100

    add passing tests for landscape functions

--- biosim/tests/test_landscape.py
+++ biosim/tests/test_landscape.py
@@ -59,6 +59,24 @@ def test_num_carns():
     assert land.num_carns == 1
 
 
+def test_update_fitness():
+    """
+    Tests if fitness is being updated for animals in the landscape.
+    """
+
+    jungle = Jungle()
+    jungle.pop_animals[0].append(Herbivore())
+    jungle.pop_animals[1].append(Carnivore())
+    f0_herb = jungle.pop_animals[0][0].phi
+    f0_carn = jungle.pop_animals[1][0].phi
+    jungle.weightloss()
+    jungle.update_fitness()
+    f1_herb = jungle.pop_animals[0][0].phi
+    f1_carn = jungle.pop_animals[1][0].phi
+    assert f0_herb > f1_herb
+    assert f0_carn > f1_carn
+
+
 def test_fitness_sort():
     """Tests that it sorts fitness correctly, descending."""
     jungle = Jungle()
@@ -106,110 +124,147 @@ def test_eat_requests_carn():
     jungle.pop_animals[0].append(Herbivore())
     jungle.pop_animals[0].append(Herbivore())
     jungle.pop_animals[0].append(Herbivore())
-    jungle.pop_animals[0].append(Herbivore())
-    jungle.pop_animals[0].append(Herbivore())
     jungle.pop_animals[1].append(Carnivore())
     jungle.fitness_sort()
 
-    carn = jungle.pop_animals[1][0]
-    w0 = carn.weight
-    weight_herb = jungle.sum_herb_mass
+    w0 = jungle.pop_animals[1][0].weight
     jungle.eat_request_carn()
+    w1 = jungle.pop_animals[1][0].weight
     assert len(jungle.pop_animals[0]) == 0
-    assert carn.weight == w0 + (weight_herb * carn.default_params['beta'])
+    assert w0 < w1
 
 
+def test_death():
+    """
+    Tests if animals die when they should.
+    """
+
+    Herbivore.set_parameters({'omega': 1})
+    Carnivore.set_parameters({'omega': 1})
+    jungle = Jungle()
+    jungle.pop_animals[0].append(Herbivore())
+    jungle.pop_animals[1].append(Carnivore())
+    jungle.pop_animals[0][0].phi = 0
+    jungle.pop_animals[1][0].phi = 0
+    jungle.death()
+    assert len(jungle.pop_animals[0]) == 0
+    assert len(jungle.pop_animals[1]) == 0
 
 
+def test_weightloss():
+    """
+    Tests if an animal looses weight following a weight loss.
+    """
+
+    jungle = Jungle()
+    jungle.pop_animals[0].append(Herbivore())
+    jungle.pop_animals[1].append(Carnivore())
+    w0_herb = jungle.pop_animals[0][0].weight
+    w0_carn = jungle.pop_animals[1][0].weight
+    jungle.weightloss()
+    w1_herb = jungle.pop_animals[0][0].weight
+    w1_carn = jungle.pop_animals[1][0].weight
+    assert w0_herb > w1_herb
+    assert w0_carn > w1_carn
 
 
+def test_reproduction_herb():
+    """
+    Tests if reproduction works and new animals are being born, when the
+    chance of birth are guaranteed. Same method for testing carnivores.
+    """
+
+    Herbivore.set_parameters({'zeta': 0})
+    jungle = Jungle()
+    jungle.pop_animals[0].append(Herbivore())
+    jungle.pop_animals[0].append(Herbivore())
+    jungle.pop_animals[0][0].default_params['gamma'] = 1
+    jungle.pop_animals[0][1].default_params['gamma'] = 1
+    jungle.pop_animals[0][0].phi = 1
+    jungle.pop_animals[0][1].phi = 1
+    n0 = len(jungle.pop_animals[0])
+    jungle.reproduction()
+    n1 = len(jungle.pop_animals[0])
+    assert n1 > n0
 
 
+def test_aging():
+    """
+    Tests if animals in the landscape ages.
+    """
+    jungle = Jungle()
+    jungle.pop_animals[0].append(Herbivore())
+    jungle.pop_animals[1].append(Carnivore())
+    jungle.aging()
+    assert jungle.pop_animals[0][0].age == 1
+    assert jungle.pop_animals[1][0].age == 1
 
 
+def test_regenerate():
+    """
+    Tests if Jungle- and Savannah landscapes regenerate fodder.
+    """
+    jungle = Jungle()
+    savannah = Savannah()
+    desert = Desert()
+    mountain = Mountain()
+    ocean = Ocean()
+
+    desert.f = 0
+    jungle.f = 0
+    savannah.f = 0
+    ocean.f = 0
+    mountain.f = 0
+
+    jungle.regenerate()
+    savannah.regenerate()
+    desert.regenerate()
+    mountain.regenerate()
+    ocean.regenerate()
+
+    assert jungle.f > 0
+    assert savannah.f > 0
+    assert desert.f == 0
+    assert mountain.f == 0
+    assert ocean.f == 0
 
 
+#def test_abundance_fodder_herb():
+#    """
+#    Tests that the abundance of fodder in
+#    Jungle > Savannah > Desert == Ocean == Mountain, when new instance.
+#    """
+#
+#    desert = Desert()
+#    jungle = Jungle()
+#    savannah = Savannah()
+#    ocean = Ocean()
+#    mountain = Mountain()
+#
+#    assert jungle.abundance_fodder_h > savannah.abundance_fodder_h
+#    assert savannah.abundance_fodder_h > desert.abundance_fodder_h
+#    assert desert.abundance_fodder_h == ocean.abundance_fodder_h
+#    assert ocean.abundance_fodder_h == mountain.abundance_fodder_h
 
-#def test_reproduction():
-#    Landscape.set_parameters({'f_max'} )
-#    land = Landscape()
-#
-#
-#
-#
-#
-#def test_reproduction():
-#    the_jungle = Jungle(100, 10)
-#    for herb in the_jungle.pop_animals[0]:
-#        herb.weight = 100.0
-#    for carn in the_jungle.pop_animals[1]:
-#        carn.weight = 100.0
-#    the_jungle.reproduction()
-#    assert len(the_jungle.pop_animals[0]) > 100
-#    assert len(the_jungle.pop_animals[1]) > 10
-#
-#
-#def test_aging():
-#    the_jungle = Jungle(1, 1)
-#    a0h = the_jungle.pop_animals[0][0].age
-#    a0c = the_jungle.pop_animals[1][0].age
-#    the_jungle.aging()
-#    assert the_jungle.pop_animals[0][0].age - a0h == 1
-#    assert the_jungle.pop_animals[1][0].age - a0c == 1
-#
-#
-#def test_weightloss():
-#    the_jungle = Jungle(1, 1)
-#    w0_herb = the_jungle.pop_animals[0][0].weight
-#    w0_carn = the_jungle.pop_animals[1][0].weight
-#
-#    the_jungle.pop_animals[0][0].weightloss()
-#    the_jungle.pop_animals[1][0].weightloss()
-#
-#    assert w0_herb > the_jungle.pop_animals[0][0].weight
-#    assert w0_carn > the_jungle.pop_animals[1][0].weight
-#
-#
-#def test_death():
-#    the_jungle = Jungle(1000, 100)
-#    the_jungle.death()
-#    assert len(the_jungle.pop_animals[0]) < 1000 and len(the_jungle.pop_animals[1]) < 100
-#
-#
-#def test_regenerate_jungle():
-#    the_jungle = Jungle(1)
-#    f0 = the_jungle.f
-#    the_jungle.eat_request()
-#    f1 = the_jungle.f
-#    assert f1 < f0
-#    the_jungle.regenerate()
-#    assert the_jungle.f == the_jungle.default_params['f_max']
-#
-#
-#def test_regenerate_savannah():
-#    Savannah.set_params({'f_max': 300})
-#    the_savannah = Savannah(1)
-#    f0 = the_savannah.f
-#    the_savannah.eat_request()
-#    f1 = the_savannah.f
-#    assert f1 < f0
-#    the_savannah.f = 0
-#    the_savannah.regenerate()
-#    assert the_savannah.f < the_savannah.default_params['f_max']
-#
-#
-#def test_eat_request():
-#    the_jungle = Jungle(100, 10)
-#    w0_herb = sum([herb.weight for herb in the_jungle.pop_animals[0]])/len(the_jungle.pop_animals[0])
-#    w0_carn = sum([carn.weight for carn in the_jungle.pop_animals[1]])
-#    the_jungle.eat_request()
-#    assert sum([herb.weight for herb in the_jungle.pop_animals[0]])/len(the_jungle.pop_animals[0]) > w0_herb
-#    assert sum([carn.weight for carn in the_jungle.pop_animals[1]]) > w0_carn
-#
-#def test_migration()
-#    kart = """OOOO
-#    OJJO
-#    OOOO"""
-#   population = [Herbivore() for i in range(100)]
-#    sim = BioSim(island_map=kart, ini_pop=population, seed=1234)
-#    sim.island.map[]
+
+def test_abundance_fodder_carn():
+    """
+    Tests that the abundance of food for Carnivores is bigger
+    in a landscape with more Herbivores.
+    """
+
+    jungle1 = Jungle()
+    jungle2 = Jungle()
+
+    jungle1.pop_animals[0].append(Herbivore())
+    jungle1.pop_animals[0].append(Herbivore())
+    jungle2.pop_animals[0].append(Herbivore())
+
+    assert jungle1.abundance_fodder_c > jungle2.abundance_fodder_c
+
+
+#def test_propensity():
+
+    
+
+
